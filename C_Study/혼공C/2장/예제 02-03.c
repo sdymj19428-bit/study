5efc555478e018a4e6a641a9386fd42d421b8a99
sdymj19428-bit <sdymj19428@gmail.com>
2026-06-22 22:07:45 +0900

    혼공c

---
+++ C_Study/혼공C/2장/예제 02-03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Be happy\n");  //Be happy를 출력하고 줄을 바꿈(\n)
+    printf("1234567890123456890\n");  //Be happy를 출력하고 줄을 바꿈(\n)
+    //My를 출력하고 탭위치로 이동(\t)후에 friend를 출력하고 줄을 바꿈(\n)
+    printf("My\tfrined\n");  
+
+    /*Goot를 출력하고 한 칸 왼쪽으로 이동(\b)해 t를 d로 바꾸고 탭위치로 이동(\t)후에 
+    chance를 출력하고 줄을 바꿈(\n)*/
+    printf("Goot\bd\tchandce\n");  
+    printf("Cow\rW\a\n");  //맨 앞으로 이동(\r)해 C를 W로 바꾸고 벨소리(\a)를 내고 줄을 바꿈(\n)
+
+    return 0;
+}
