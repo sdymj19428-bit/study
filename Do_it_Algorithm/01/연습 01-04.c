e62c2f6382f63ae72d6a4e8c04615c091d072356
sdymj19428-bit <sdymj19428@gmail.com>
2026-06-24 23:18:35 +0900

    do it algorithm

---
+++ Do_it_Algorithm/01/연습 01-04.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+//a, b, c의 중앙값 반환
+int mid(int a, int b, int c)
+{
+    if(a > b)
+    {
+        if(b > c)
+        {
+            return b;
+        }
+        else if(a > c)
+        {
+            return c;
+        }
+        else
+        {
+            return a;
+        }
+    }
+    else if(a > c)
+    {
+        return a;
+    }
+    else if(b > c) 
+    {
+        return c;
+    }
+    else if(b >= c)
+    {
+        return b;
+    }
+}
+
+int main(void)
+{
+    printf("mid(%d, %d, %d) = %d\n", 3, 2, 1, mid(3, 2, 1));
+    printf("mid(%d, %d, %d) = %d\n", 3, 2, 2, mid(3, 2, 2));
+    printf("mid(%d, %d, %d) = %d\n", 3, 1, 2, mid(3, 1, 2));
+    printf("mid(%d, %d, %d) = %d\n", 3, 2, 3, mid(3, 2, 3));
+    printf("mid(%d, %d, %d) = %d\n", 2, 1, 3, mid(2, 1, 3));
+    printf("mid(%d, %d, %d) = %d\n", 3, 3, 2, mid(3, 3, 2));
+    printf("mid(%d, %d, %d) = %d\n", 3, 3, 3, mid(3, 3, 3));
+    printf("mid(%d, %d, %d) = %d\n", 2, 2, 3, mid(2, 2, 3));
+    printf("mid(%d, %d, %d) = %d\n", 2, 3, 1, mid(2, 3, 1));
+    printf("mid(%d, %d, %d) = %d\n", 2, 3, 2, mid(2, 3, 2));
+    printf("mid(%d, %d, %d) = %d\n", 1, 3, 2, mid(1, 3, 2));
+    printf("mid(%d, %d, %d) = %d\n", 2, 3, 3, mid(2, 3, 3));
+    printf("mid(%d, %d, %d) = %d\n", 1, 2, 3, mid(1, 2, 3));
+
+    return 0;
+}
