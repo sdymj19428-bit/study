ecdc3c8287911bc2d1f0f04d98b915563e1dd41a
sdymj19428-bit <sdymj19428@gmail.com>
2026-06-26 01:05:40 +0900

    code up

---
+++ Cording_Test/Code_up/C/1019.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int year, month, day;
+
+    scanf("%d.%d.%d", &year, &month, &day);
+
+    printf("%04d.%02d.%02d", year, month, day);
+
+    return 0;
+}
