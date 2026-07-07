ce14fa3a59da90a0b6f1df1dd54b2c7bfb92525b
sdymj19428-bit <sdymj19428@gmail.com>
2026-07-07 22:16:22 +0900

    do it algorithm

---
+++ Do_it_Algorithm/01/실습 01-10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("---곱셈표---\n");
+    for(int i = 1; i <= 9; i++) {
+        for(int j = 1; j <= 9; j++) {
+            printf("%3d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
