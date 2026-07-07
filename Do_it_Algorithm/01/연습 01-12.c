43f198dd1be0676a95521c2f8ba646be3863b29d
sdymj19428-bit <sdymj19428@gmail.com>
2026-07-08 00:28:38 +0900

    do it algorithm

---
+++ Do_it_Algorithm/01/연습 01-12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("   |");
+    for(int i = 1; i <= 9; i++) {
+        printf("%3d", i);
+    }
+
+    printf("\n---+-----------------------------\n");
+
+    for(int i = 1; i <=9; i++) {
+        printf("%2d |", i);
+        for(int j = 1; j <= 9; j++) {
+            printf("%3d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
