8fe9f51786a25219c474503529d594756c1960b6
sdymj19428-bit <sdymj19428@gmail.com>
2026-07-22 01:37:50 +0900

    code up

---
+++ Cording_Test/C/code up/1073.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    while(1) {
+        scanf("%d", &num);
+        if(num == 0) {
+            break;
+        }
+        printf("%d\n", num);
+    }
+
+    return 0;
+}
